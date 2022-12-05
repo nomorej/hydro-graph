2ca8426b7721f8b8b48ace247bd468e97e485f8b
nomorejalapenos <nomorejalapeno@gmail.com>
2022-12-05 15:15:43 +0300

    print title

--- src/plugins/Print.ts
+++ src/plugins/Print.ts
@@ -52,56 +52,65 @@ export class Print extends Extension {
 
     if (win) {
       win.document.write(`
-        <style>
-          * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-          }
-          html, body {
-            width: 100%;
-            height: 100%;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-          }
-          img {
-            width: 100%;
-            height: 100%;
-            object-fit: contain;
-          }
-          button {
-            position: fixed;
-            top: 2vmin;
-            right: 2vmin;
-            z-index: 1;
-            width: 10vmin;
-            height: 10vmin;
-            padding: 2vmin;
-            background: none;
-            border: none;
-            border: 0.2vmin solid #4C6EF5;
-            border-radius: 50%;
-            cursor: pointer;
-          }
-          @media print {
-            button {
-              display: none;
+        <head>
+          <title>Ледовая линейка</title>
+          <style>
+            * {
+              margin: 0;
+              padding: 0;
+              box-sizing: border-box;
             }
-          }
-        </style>
-        <img src="${url}"/>
-        <button><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 45 45">
-        <path d="M42.5,19.408H40V1.843c0-0.69-0.561-1.25-1.25-1.25H6.25C5.56,0.593,5,1.153,5,1.843v17.563H2.5
-          c-1.381,0-2.5,1.119-2.5,2.5v20c0,1.381,1.119,2.5,2.5,2.5h40c1.381,0,2.5-1.119,2.5-2.5v-20C45,20.525,43.881,19.408,42.5,19.408z
-          M32.531,38.094H12.468v-5h20.063V38.094z M37.5,19.408H35c-1.381,0-2.5,1.119-2.5,2.5v5h-20v-5c0-1.381-1.119-2.5-2.5-2.5H7.5
-          V3.093h30V19.408z M32.5,8.792h-20c-0.69,0-1.25-0.56-1.25-1.25s0.56-1.25,1.25-1.25h20c0.689,0,1.25,0.56,1.25,1.25
-          S33.189,8.792,32.5,8.792z M32.5,13.792h-20c-0.69,0-1.25-0.56-1.25-1.25s0.56-1.25,1.25-1.25h20c0.689,0,1.25,0.56,1.25,1.25
-          S33.189,13.792,32.5,13.792z M32.5,18.792h-20c-0.69,0-1.25-0.56-1.25-1.25s0.56-1.25,1.25-1.25h20c0.689,0,1.25,0.56,1.25,1.25
-          S33.189,18.792,32.5,18.792z" stroke="#4C6EF5" fill="#4C6EF5"/>
-      </svg></button>
+            html, body {
+              width: 100%;
+              height: 100%;
+              display: flex;
+              align-items: center;
+              justify-content: center;
+            }
+            img {
+              width: 100%;
+              height: 100%;
+              object-fit: contain;
+            }
+            button {
+              position: fixed;
+              top: 2vmin;
+              right: 2vmin;
+              z-index: 1;
+              width: 10vmin;
+              height: 10vmin;
+              padding: 2vmin;
+              background: none;
+              border: none;
+              border: 0.2vmin solid #4C6EF5;
+              border-radius: 50%;
+              cursor: pointer;
+            }
+            @media print {
+              button {
+                display: none;
+              }
+            }
+          </style>
+        </head>
+        <body>
+          <img src="${url}"/>
+          <button>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 45 45">
+              <path d="M42.5,19.408H40V1.843c0-0.69-0.561-1.25-1.25-1.25H6.25C5.56,0.593,5,1.153,5,1.843v17.563H2.5
+                c-1.381,0-2.5,1.119-2.5,2.5v20c0,1.381,1.119,2.5,2.5,2.5h40c1.381,0,2.5-1.119,2.5-2.5v-20C45,20.525,43.881,19.408,42.5,19.408z
+                M32.531,38.094H12.468v-5h20.063V38.094z M37.5,19.408H35c-1.381,0-2.5,1.119-2.5,2.5v5h-20v-5c0-1.381-1.119-2.5-2.5-2.5H7.5
+                V3.093h30V19.408z M32.5,8.792h-20c-0.69,0-1.25-0.56-1.25-1.25s0.56-1.25,1.25-1.25h20c0.689,0,1.25,0.56,1.25,1.25
+                S33.189,8.792,32.5,8.792z M32.5,13.792h-20c-0.69,0-1.25-0.56-1.25-1.25s0.56-1.25,1.25-1.25h20c0.689,0,1.25,0.56,1.25,1.25
+                S33.189,13.792,32.5,13.792z M32.5,18.792h-20c-0.69,0-1.25-0.56-1.25-1.25s0.56-1.25,1.25-1.25h20c0.689,0,1.25,0.56,1.25,1.25
+                S33.189,18.792,32.5,18.792z" stroke="#4C6EF5" fill="#4C6EF5"/>
+            </svg>
+          </button>
+        </body>
       `)
+
       const button = win.document.querySelector('button')
+
       button?.addEventListener('click', () => {
         win.setTimeout(() => win.print(), 0)
       })
